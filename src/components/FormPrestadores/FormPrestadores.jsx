47e1a3f37be7bde25ac1e959f265d1a2f1489e6f
SantiagoSolari <santiagosolariivan@gmail.com>
2025-10-01 17:23:47 -0300

    Cambios en pantalla Prestadores

--- src/components/FormPrestadores/FormPrestadores.jsx
+++ src/components/FormPrestadores/FormPrestadores.jsx
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { FaSearch } from "react-icons/fa";
 import './FormPrestadores.css'
-import prestadores from '../../db/prestadores.js'
+
 
 
 const FormPrestadores = (props) => {
@@ -14,29 +14,29 @@ const FormPrestadores = (props) => {
         <>
             <Form onSubmit={(e) => props.buscarPrestadores(e)} className="formPrestadores" >
                 <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Group as={Col} controlId="formNombrePrestador">
                         <Form.Label>Buscar por nombre</Form.Label>
                         <Form.Control type="text" placeholder="Juan Gomez / Centro Medico BA" />
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>Especialidad<span >*</span></Form.Label>
+                    <Form.Group as={Col} controlId="formEspecialidadPrestador">
+                        <Form.Label>Especialidad<span style={{color: 'red'}}>*</span></Form.Label>
                         <Form.Select onChange={(e) => props.especialidad(e.target.value)} defaultValue="" >
-                            <option className="options" value={"Cardiologia"}>Cardiologia</option>
-                            <option className="options" value={"Diabetologia"}>Diabetologia</option>
-                            <option className="options" value={"Psicologia"}>Psicologia</option>
+                            <option className="options" value={"Cardiología"}>Cardiología</option>
+                            <option className="options" value={"Diabetología"}>Diabetología</option>
+                            <option className="options" value={"Psicología"}>Psicología</option>
                         </Form.Select>
                     </Form.Group>
                 </Row>
 
-                <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Group className="mb-3" controlId="formUbicacionPrestador">
                     <Form.Label>Ubicación</Form.Label>
                     <Form.Control placeholder="1234 Main St" />
                 </Form.Group>
 
                 <Row className="mb-3">
 
-                    <Form.Group as={Col} controlId="formGridState">
+                    <Form.Group as={Col} controlId="formTipoPrestador">
                         <Form.Label>Tipo de Prestador</Form.Label>
                         <Form.Select defaultValue="Tipo de Prestador">
                             <option className="options">Tipo de Prestador</option>
