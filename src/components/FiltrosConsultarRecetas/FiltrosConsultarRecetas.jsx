00caf25589d73c9eac1cc9189a75588e47bbda24
VarelaValentin <valentinadriel.varela@estudiantes.unahur.edu.ar>
2025-10-04 18:52:55 -0300

    Nuevo: Opciones de filtros dinámicos Ahora los filtros de la pantalla de Consulta de Recetas muestra las opciones según la información en pantalla  - Cambios: Pequeños cambios en el css

--- src/components/FiltrosConsultarRecetas/FiltrosConsultarRecetas.jsx
+++ src/components/FiltrosConsultarRecetas/FiltrosConsultarRecetas.jsx
@@ -1,34 +1,30 @@
-import React, {useState} from "react";
+import React from "react";
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './FiltrosConsultarRecetas.css'
 
 const FiltrosConsultarRecetas = (props) => {
-
+    
     return (
-        <>
-            {/* {console.log(props)} */}
-            <Form>
-
-                <Form.Group as={Col}>
-                    <Form.Label>{props.label}</Form.Label>
-                    <div className="selectYBotonContainer">
-                        <Form.Select value={props.valorActual} onChange={e => props.filtrarAlSeleccionar(e.target.value)}>
-                            <option value="" disabled={props.defaultDesactivado}>{props.default}</option>
-                            {props.opciones.map((opcion, idx) => (<option key={idx} value={opcion}>{opcion}</option>))}
-                        </Form.Select>
-                        <Button
-                            className="botonLimpiar"
-                            onClick={()=> {props.borrarFiltro();}}
-                            type="button"
-                        >
-                            Limpiar
-                        </Button>
-                    </div>
-                </Form.Group>
-            </Form>
-        </>
+        <Form>
+            <Form.Group as={Col}>
+                <Form.Label htmlFor={`select-de-${props.label}`}>{props.label}</Form.Label>
+                <div className="selectYBotonContainer">
+                    <Form.Select id={`select-de-${props.label}`} value={props.valorActual} onChange={e => props.filtrarAlSeleccionar(e.target.value)}>
+                        <option value="" disabled={props.defaultDesactivado}>{props.default}</option>
+                        {props.opciones.map((opcion, idx) => (<option key={idx} value={opcion}>{opcion}</option>))}
+                    </Form.Select>
+                    <Button
+                        className="botonLimpiar"
+                        onClick={() => { props.borrarFiltro(); }}
+                        type="button"
+                    >
+                        Limpiar
+                    </Button>
+                </div>
+            </Form.Group>
+        </Form>
     )
 }
 
