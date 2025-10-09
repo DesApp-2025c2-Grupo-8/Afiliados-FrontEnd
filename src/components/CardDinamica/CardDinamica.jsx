15635a8256e93b907414afec5b2c8396c5cb13dc
VarelaValentin <valentinadriel.varela@estudiantes.unahur.edu.ar>
2025-10-09 15:51:29 -0300

    Cambios: Css de Pantallas y componentes de consultas de recetas cambiados a Modules Css     - Pequeño ajuste para mostrar las recetas de más nuevas a más antiguas Borrado: CardRecetas, ya que ahora se usa CardDinamica

--- src/components/CardDinamica/CardDinamica.jsx
+++ src/components/CardDinamica/CardDinamica.jsx
@@ -1,28 +1,28 @@
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CardDinamica.css'
+import styles from './CardDinamica.module.css';
 import { FiDownload } from 'react-icons/fi';
 
 const CardDinamica = (props) => {
   const { data, header, color, camposCard, tieneBotonDescarga } = props;
 
   return (
-    <Card className='card-box'>
+    <Card className={styles.cardBox}>
       {/* {console.log(props)} */}
-      <Card.Header className={`card-header ${color}`}>
+      <Card.Header className={`${styles.cardHeader} ${color}`}>
         {header}
       </Card.Header>
       <Card.Body>
         {camposCard.map(({ campo, propiedad }) => (
-          <Card.Text className='card-texto' key={propiedad}>
+          <Card.Text className={styles.cardTexto} key={propiedad}>
             {/* {console.log(data[key])} */}
             <strong>{campo}: </strong>{data[propiedad]}
           </Card.Text>
         ))}
 
         {tieneBotonDescarga && (
-          <div className='boton-box'>
-            <p className='botonPDF'><FiDownload style={{ marginRight: '10px' }} />Descargar</p>
+          <div className={styles.botonBox}>
+            <p className={styles.botonPDF}><FiDownload style={{ marginRight: '10px' }} />Descargar</p>
           </div>
         )}
       </Card.Body>
