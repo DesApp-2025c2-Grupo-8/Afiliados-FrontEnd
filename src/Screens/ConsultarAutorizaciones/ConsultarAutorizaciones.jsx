435aaf4495eb0967ba036de4d4c46887e49877e2
SantiagoSolari <santiagosolariivan@gmail.com>
2025-10-11 18:42:08 -0300

    Correcciones en pantallaAutorizaciones

--- src/Screens/ConsultarAutorizaciones/ConsultarAutorizaciones.jsx
+++ src/Screens/ConsultarAutorizaciones/ConsultarAutorizaciones.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 import autorizaciones from '../../db/autorizaciones.js'
 import CardDinamica from '../../components/CardDinamica/CardDinamica.jsx';
 import styles from './ConsultarAutorizaciones.module.css'
-import { use } from "react";
+import FiltrosCards from '../../components/FiltrosCards/FiltrosCards';
+import { MdCancel } from 'react-icons/md';
+import { BsClipboard2Plus } from 'react-icons/bs';
 
 
 const autorizacionesOrdenInverso = [...autorizaciones].reverse();
@@ -44,22 +47,22 @@ const ConsultarAutorizaciones = () => {
 
     const filtrarPorMedico = (unMedico) => {
         setFiltroMedico(unMedico);
-        aplicarFiltros(unMedico, filtroIntegrante, filtroEstado, filtroPeriodo);
+        aplicarFiltros(unMedico, filtroIntegrante, filtroPeriodo, filtroEstado);
     }
 
     const filtrarPorIntegrante = (unIntegrante) => {
         setFiltroIntegrante(unIntegrante);
-        aplicarFiltros(filtroMedico, unIntegrante, filtroEstado, filtroPeriodo);
+        aplicarFiltros(filtroMedico, unIntegrante, filtroPeriodo, filtroEstado);
     }
 
     const filtrarPorPeriodo = (unPeriodo) => {
         setFiltroPeriodo(unPeriodo);
-        aplicarFiltros(filtroMedico, filtroIntegrante, filtroEstado, unPeriodo);
+        aplicarFiltros(filtroMedico, filtroIntegrante, unPeriodo, filtroEstado);
     }
 
     const filtrarPorEstado = (unEstado) => {
         setFiltroEstado(unEstado);
-        aplicarFiltros(filtroMedico, filtroIntegrante, unEstado, filtroPeriodo);
+        aplicarFiltros(filtroMedico, filtroIntegrante, filtroPeriodo, unEstado);
     }
 
     const aplicarFiltros = (unMedico, unIntegrante, unPeriodo, unEstado) => {
@@ -82,17 +85,17 @@ const ConsultarAutorizaciones = () => {
         setListaAutorizacionesFiltradas(listaAutorizacionesAFiltrar);
 
         const opcionesMedicos = listaAutorizaciones.filter(autorizacion =>
-            (!unIntegrante || autorizacion.integrante.toLowerCase().includes(unMedico.toLowerCase())) &&
+            (!unIntegrante || autorizacion.integrante === unIntegrante) &&
             (!unEstado || autorizacion.estado === unEstado)
         ).map(autorizacion => autorizacion.medico)
 
         const opcionesIntegrantes = listaAutorizaciones.filter(autorizacion =>
-            (!unMedico || autorizacion.medico.toLowerCase().includes(unMedico.toLowerCase())) &&
+            (!unMedico || autorizacion.medico === unMedico) &&
             (!unEstado || autorizacion.estado === unEstado)
         ).map(autorizacion => autorizacion.integrante)
 
         const opcionesEstados = listaAutorizaciones.filter(autorizacion =>
-            (!unMedico || autorizacion.medico.toLowerCase().includes(unMedico.toLowerCase())) &&
+            (!unMedico || autorizacion.medico === unMedico) &&
             (!unIntegrante || autorizacion.integrante === unIntegrante)
         ).map(autorizacion => autorizacion.estado);
 
@@ -206,16 +209,14 @@ const ConsultarAutorizaciones = () => {
     return (
         <>
             <div className={styles.containerConsultarAutorizaciones}>
-                <h1 className={styles.tituloResultadoAutorizaciones}>Consulta Autorizaciones</h1>
-
-                <section className={styles}>
+                <section className={styles.botonesContainer}>
                     <h1>Consultar Autorizaciones</h1>
-                    <Link className={styles} to={'/cargar-autorizacion'}><BsClipboard2Plus style={{ marginRight: '10px' }} />Cargar Autorizacion</Link>
+                    <Link className={styles.botonCargarReceta} to={'/cargar-autorizacion'}><BsClipboard2Plus style={{ marginRight: '10px' }} />Cargar Autorizacion</Link>
                 </section>
 
                 <div className={styles.box}>
                     <section className={styles.filtroContainer}>
-                        <h2>Filtrar recetas por:</h2>
+                        <h2>Filtrar Autorizaciones por:</h2>
                         <hr />
                         <div className={styles.botonLimpiarFiltrosContainer}>
                             <button className={styles.botonLimpiarFiltros} onClick={limpiarFiltros}>Limpiar filtros<MdCancel style={{ marginLeft: '10px' }} /></button>
@@ -224,13 +225,12 @@ const ConsultarAutorizaciones = () => {
                             <FiltrosCards {...unFiltro} key={unFiltro.label} />
                         ))}
                         <hr />
-                        <h3>{listaRecetasFiltradas.length} receta(s) encontradas</h3>
+                        <h3>{listaAutorizacionesFiltradas.length} Autorizacion(es) encontradas</h3>
                     </section>
 
-
-                    <section className={styles.containerResultadoAutorizaciones}>
+                    <section className={styles.recetasContainer}>
                         <h2 className={styles.tituloResultadoAutorizaciones}>Resultados de Búsqueda</h2>
-                        {autorizaciones.length > 0 ? (autorizaciones.map((autorizacion, idx) => (
+                        {listaAutorizacionesFiltradas.length > 0 ? (listaAutorizacionesFiltradas.map((autorizacion, idx) => (
                             <CardDinamica
                                 {...cardData}
                                 color={autorizacion.estado}
